69d8cd406593248392452650aad0a8bd8aeeca36
Pedro Leite <pedrohleite28@gmail.com>
2025-03-27 21:00:42 -0300

    Removendo dependencias do PrimeVue

--- resources/js/Pages/Frota/IndexFrota.vue
+++ resources/js/Pages/Frota/IndexFrota.vue
@@ -1,23 +1,6 @@
-
-<script setup lang="ts">
-    import {
-        Button,
-        Card,
-    } from 'primevue';
-</script>
-
 <template>
-    <div>
-        <h1>Testando PrimeVue</h1>
-        <Button label="Clique Aqui" icon="pi pi-check" severity="danger" />
-    </div>
-    <Button label="Secondary" severity="secondary" />
-    <Card>
-        <template  #title>Card teste</template>
-        <template #content>
-            Corpo do card de teste
-        </template>
-    </Card>
+    Hello word!!!!!
 </template>
 
-
+<script setup>
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,6 @@ import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
-import App from './App.vue';
-import PrimeVue from 'primevue/config';
-
-// import 'primevue/resources/themes/lara-light-blue/theme.css';
-// import 'primevue/resources/primevue.min.css';
-// import 'primeicons/primeicons.css';
-
 createInertiaApp({
     resolve: (name) =>
         resolvePageComponent(
@@ -16,10 +9,6 @@ createInertiaApp({
             import.meta.glob('./Pages/**/*.vue')
         ),
     setup({ el, App, props }) {
-        const app = createApp({ render: () => h(App, props) });
-        
-        app.use(PrimeVue);
-
-        app.mount(el);
+        createApp({ render: () => h(App, props) }).mount(el);
     },
 });

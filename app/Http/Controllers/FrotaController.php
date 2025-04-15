be0a126554504aa04aae32ce01ab204fe8b95d1e
Pedro Leite <pedrohleite28@gmail.com>
2025-04-14 21:12:43 -0300

    Instalando e configurando biblioteca de graficos

--- app/Http/Controllers/FrotaController.php
+++ app/Http/Controllers/FrotaController.php
@@ -14,52 +14,16 @@ class FrotaController extends Controller
     {
         return Inertia::render('Frota/IndexFrota');
     }
+    
+    // public function create(){}
+    // public function store(Request $request){}
+    // public function show(string $id){}
+    // public function edit(string $id){}
+    // public function update(Request $request, string $id){}
+    // public function destroy(string $id){}
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
+    public function index_grafico()
     {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return Inertia::render('testandoGrafico');
     }
 }
